cc96820ed41899c768dd4f7b932b8f0fe3340d3a
Vijay P V <pvvijayraj1998@gmail.com>
2023-07-24 09:48:26 +0530

    Initial commit on project creation

---
+++ src/main/java/com/hacked/springsecurity6oauth2client/SpringSecurity6OAuth2ClientApplication.java
@@ -0,0 +1,13 @@
+package com.hacked.springsecurity6oauth2client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurity6OAuth2ClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurity6OAuth2ClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hacked/springsecurity6oauth2client/SpringSecurity6OAuth2ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hacked.springsecurity6oauth2client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6OAuth2ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
