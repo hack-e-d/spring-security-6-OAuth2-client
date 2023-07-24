82c0825969841a2edffedfab9919000ec88317b7
vijay <Vijay.PV@expeditors.com>
2023-07-24 17:11:05 +0530

    Complete client implementation

--- src/main/java/com/hacked/springsecurity6oauth2client/Configs/SecurityConfig.java
+++ src/main/java/com/hacked/springsecurity6oauth2client/Configs/SecurityConfig.java
@@ -4,10 +4,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableMethodSecurity(prePostEnabled = false)
+@EnableMethodSecurity()
 public class SecurityConfig {
 
     @Bean
@@ -17,7 +30,44 @@ public class SecurityConfig {
                 .oauth2Client()
                 .and()
                 .authorizeHttpRequests()
-                .requestMatchers("/api/getToken").permitAll();
+                .anyRequest().permitAll();
         return httpSecurity.build();
+    }
+
+    @Bean
+    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository
+    ) {
+        OAuth2AuthorizedClientProvider provider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .authorizationCode()
+                .refreshToken()
+                .clientCredentials()
+                .build();
+
+        DefaultOAuth2AuthorizedClientManager defaultOAuth2AuthorizedClientManager = new DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, auth2AuthorizedClientRepository);
+        defaultOAuth2AuthorizedClientManager.setAuthorizedClientProvider(provider);
+        return defaultOAuth2AuthorizedClientManager;
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+
+        ClientRegistration clientRegistration = ClientRegistration.withRegistrationId("1")
+                .clientId("client")
+                .clientSecret("secret")
+                .authorizationGrantType(AuthorizationGrantType.CLIENT_CREDENTIALS)
+                .clientAuthenticationMethod(ClientAuthenticationMethod.CLIENT_SECRET_BASIC)
+                .tokenUri("http://localhost:3000/oauth2/token")
+                .scope(OidcScopes.OPENID)
+                .build();
+
+        InMemoryClientRegistrationRepository clientRegistrationRepository = new InMemoryClientRegistrationRepository(clientRegistration);
+        return clientRegistrationRepository;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(8);
     }
 }
--- src/main/java/com/hacked/springsecurity6oauth2client/Controllers/DemoController.java
+++ src/main/java/com/hacked/springsecurity6oauth2client/Controllers/DemoController.java
@@ -1,18 +1,34 @@
 package com.hacked.springsecurity6oauth2client.Controllers;
 
+import org.springframework.security.oauth2.client.OAuth2AuthorizeRequest;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/api")
+@RestController()
 public class DemoController {
+
+    private final OAuth2AuthorizedClientManager auth2AuthorizedClientManager;
+
+    public DemoController(OAuth2AuthorizedClientManager auth2AuthorizedClientManager) {
+        this.auth2AuthorizedClientManager = auth2AuthorizedClientManager;
+    }
 
     /*
     * Get token endpoint fetches the bearer token from the authorization server
     * @author vijay pv
     * @return bearer token
     * */
-    @GetMapping("/getToken")
+    @GetMapping("/token")
     public String getToken() {
-        return "";
+
+        OAuth2AuthorizeRequest request = OAuth2AuthorizeRequest
+                .withClientRegistrationId("1")
+                .principal("client")
+                .build();
+
+        OAuth2AuthorizedClient client = this.auth2AuthorizedClientManager.authorize(request);
+        return client.getAccessToken().getTokenValue(); // added on the Authorization header on the request with prefix "Bearer..."
     }
 }
